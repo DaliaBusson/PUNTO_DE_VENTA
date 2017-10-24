fe51f27dc23c1b59c8f780e752a975d0395ef808
DulceQuiroz <32169895+DulceQuiroz@users.noreply.github.com>
2017-10-24 16:33:22 -0500

    DulceQuiroz

---
+++ conexion2/src/conexion1/CONEXION1.java
@@ -0,0 +1,19 @@
+package conexion1; //PAQUETERIA DEL PROGRAMA
+ import java.awt.Image;
+ import java.awt.Toolkit;
+
+public class CONEXION1 {  //CLASE PRINCIPAL
+    
+public Image getIconImage() {
+ Image retValue = Toolkit.getDefaultToolkit().getImage(ClassLoader.getSystemResource("conexion1.IMAGENES/logohp.png"));
+ return retValue;
+ }
+    boolean stado=false; //VARIABLE UTILIADA PARA CONTROL DE ACCESO
+    
+    public static void main(String[] args) {
+        
+    LOGIN windows=new LOGIN();
+    windows.setVisible(true);     //abilita la ventana DE LOGIN
+    }
+}
+// MODULO TERMINADO!!!
